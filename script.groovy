22ce3d15b4d44f88f1654a9c3c3871b13ee1940d
FRCDMX <fer_darkwizard@hotmail.com>
2017-08-22 23:11:47 -0500

    Modificacion de script

--- script.groovy
+++ script.groovy
@@ -1,26 +1,44 @@
+import groovy.json.JsonSlurper
+
 def array = new File("archivo.txt") as String[]
 def matriz=new String [array.length][]
 for(i=0;i<array.length;i++){
     matriz[i]=array[i].split("[,]")
 }
-def file = new File("salida.json").withWriter{ writer ->
-    writer.writeLine "["
+def file
+    file= "[ \n"
     for(i=1;i<array.length;i++){
-        writer.writeLine "    {"
+        file=file + "    { \n"
         for(j=0;j<matriz[i].length;j++){
-             writer.write "        \""+ matriz[0][j]+"\":\""+matriz[i][j]+"\""
+             file=file+ "        \""+ matriz[0][j]+"\":\""+matriz[i][j]+"\""
              if (j!=(matriz[i].length-1))
-                 writer.writeLine ","
+                 file=file+ ",\n"
         }
-        writer.writeLine ""
-        writer.write "    }"
+        file=file+ "\n"
+        file=file+ "    }"
         if (i!=(array.length-1))
-            writer.write ","
-         writer.writeLine ""
+            file=file+ ","
+         file=file+ "\n"
     }
-    writer.writeLine "]"
- }
- def archivo = new File("salida.json")
-    archivo.eachLine { linea ->
-    println linea
-}
+    file=file+ "]"
+    
+    def jsonSlurper=new JsonSlurper()
+    def object = jsonSlurper.parseText(file)
+    
+//Las siguientes son notas    
+    
+    println object.size() //regresa el número de objetos
+    
+    println object[1].keySet() //Regresa la lista de key (encabezado)
+    
+    println object[1].keySet()[1] //Regresa el key en la posición
+    
+    println object[1].size() //regresa el número de atributos del objeto
+    
+    println object[0].values() //Regresa la lista de valores del objeto seleccionado
+    
+    println object[0].values()[1] //Muestra el valor seleccionado para el objeto seleccionado
+    
+    println object[0].(object[1].keySet()[2]) //Muestra el valor del key seleccionado para el objeto seleccionado
+    
+    
